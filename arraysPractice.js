047de43b4ac48a5f9e73a9fe1f2012c8d1514822
Steven Nagie <sanagie@yahoo.com>
2016-10-15 13:40:13 -0600

    Work completed at home and SLC library by 2 pm on Saturday the 15th

--- arraysPractice.js
+++ arraysPractice.js
@@ -5,6 +5,9 @@ var arr = [10,20,30];
 //Create a function named 'first' that is given 'arr' as the argument and  returns the first item in the given array.
 
   //Code Here
+  function first(arr){
+    return arr[0];
+  }
 
 
 //Next problem
@@ -16,6 +19,9 @@ var arr = [40,50,60];
 
 
   //Code Here
+  function last(arr){
+    return arr[arr.length - 1];
+  }
 
 
 //Next Problem
@@ -25,7 +31,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 //Create a function named 'looper' that is given family as it's only argument, loops through the given array, and alerts every item in the array.
 
   //Code Here
-
+  function looper(family){
+    for (var i = 0; i < family.length; i++){
+      alert(arr[i]);
+    }
+  }
 
 //Next problem
 
@@ -35,6 +45,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 //Write a function called reversedLooper that is given letters as it's only argument and loops through the given array backwards alerting every item in the array starting at the end.
 
   //Code Here
+  function reversedLooper(letters){
+    for (var i = letters.length - 1; i >= 0; i--){
+      alert(letters[i]);
+    }
+  }
 
 
 //Next Problem
@@ -44,6 +59,13 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 //Write a function named evenFinder that is given nums as it's only argument and removes all values that aren't even from the given array.
 
   //Code Here
+  function evenFinder(nums){
+    for (var i = 0; i < nums.length; i++){
+      if (nums[i] % 2 === 1){
+        nums.splice(i, 1);
+      }
+    }
+  }
 
 
 //Next problem
@@ -56,7 +78,19 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 
 
   //Code Here
-
+  function divider(numbersArray){
+    var arr2 = [];
+    arr2[0] = [];
+    arr2[1] = [];
+    for (var i = 0; i < numbersArray.length; i++){
+      if(numbersArray[i] % 2 === 0){
+        arr2[0].push(numbersArray[i]);
+      } else {
+        arr2[1].push(numbersArray[i]);
+      }
+    }
+    return arr2;
+  }
 
 //Next Problem
 
@@ -70,6 +104,16 @@ var getRandomArbitrary = function() {
 // Your job is to write a function named finder that will get a random number (by invoking getRandomArbitrary), then loop through the array (that will be passed in as a parameter) to see if that random number is in the array. If it is, return true, if it's not, return false
 
   //Code Here
+  function finder(randomArr){
+    var randomNum = getRandomArbitrary();
+
+    for (var i = 0; i < randomArr.length; i++){
+      if (randomArr[i] === randomNum){
+        return true;
+      }
+    }
+    return false;
+  }
 
   //Code Here
 
@@ -82,6 +126,9 @@ var str = 'this is my sentence';
 //Write a function called reverse that takes a given str as it's only argument and returns that string after it's been reversed
 
   //Code Here
+  function reverse(str){
+    str = str.split("").reverse().join("");
+  }
 
 
 //Next Problem
